8b5a1677aae1fc55bf4fbd067442cd62fa2aae73
kaiostavares <kaiosandek@outlook.com>
2024-04-04 22:03:57 -0300

    add tableComponents & buttons + page dynamism

---
+++ src/components/nav-link.tsx
@@ -0,0 +1,10 @@
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface NavLinkProps extends ComponentProps<'a'>{}
+
+export function NavLink(props: NavLinkProps){
+  return(
+    <a {...props} className={twMerge("font-medium text-sm",props.className)}>{props.children}</a>
+  )
+}
